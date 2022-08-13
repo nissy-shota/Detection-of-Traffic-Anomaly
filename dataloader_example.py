73661dd5c3ab83e260477d14634b56fc33bdbcbe
nissy-shota <shota_4410@icloud.com>
2022-08-13 18:34:13 +0900

    Fix to without using argparse and Disp dataloader

--- dataloader_example.py
+++ dataloader_example.py
@@ -1,10 +1,45 @@
-"""
-This is an example for generating dataset object for FOL model training and evalutaion.
-"""
+from typing import Dict
+
+import yaml
+
 from dataset.dota import DoTADataset
-from config.config import parse_args
 
-args = parse_args()
 
-# initialize the dataset object used to train the model
-dataset = DoTADataset(args, phase="train")
+def load_yaml(yaml_file: str) -> Dict:
+    """
+    Load yaml file.
+    """
+    with open(yaml_file) as stream:
+        param = yaml.safe_load(stream)
+    return param
+
+
+def main():
+
+    config = load_yaml("config/config_example.yaml")
+    phase = "train"
+    dota_dataset = DoTADataset(config=config, phase=phase)
+    print("beautiful world")
+
+    for i, (
+        input_bbox,
+        input_flow,
+        input_ego_motion,
+        target_bbox,
+        target_ego_motion,
+    ) in enumerate(dota_dataset):
+
+        print(f"{phase} dataset length: {len(dota_dataset)}")
+
+        print(f"loop {i+1}")
+        print(f"input_bbox: {input_bbox.shape}")
+        print(f"input_flow: {input_flow.shape}")
+        print(f"input_ego_motion: {input_ego_motion.shape}")
+        print(f"target_bbox: {target_bbox.shape}")
+        print(f"input_bbox: {target_ego_motion.shape}")
+
+        break  # for debug
+
+
+if __name__ == "__main__":
+    main()
